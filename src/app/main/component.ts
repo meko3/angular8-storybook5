53707e61ceba7a7a851cc9ba1129f25eadc55e60
unknown <88240@glory.local>
2019-07-23 18:06:48 +0900

    [docs] add scss

--- src/app/main/component.ts
+++ src/app/main/component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "main",
-  templateUrl: "./component.html"
+  templateUrl: "./component.html",
+  styleUrls: ["./component.scss"]
 })
 export class MainComponent {
   title = "sample-project";
