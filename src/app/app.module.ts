6a83ec54f155939c0cc8c2eac1635e59299976fa
meko3 <hirono.yoneyama@outlook.com>
2020-01-08 13:12:48 +0900

    [feat] add gear component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { ComponentsModule } from "./components.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MainComponent } from "./main/component";
+import { GearComponent } from "./gear/gear.component";
 
 @NgModule({
-  declarations: [AppComponent, MainComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, MainComponent, GearComponent],
+  imports: [BrowserModule, AppRoutingModule, ComponentsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
